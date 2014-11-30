7337f531c046d39927074354cfaed60ea87773a1
NE- <enriquenajera94@yahoo.com>
2014-11-30 11:22:06 -0800

    Project 2

---
+++ Proj/Project 2/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * File:    main.cpp
+ * Author:  Enrique Najera
+ * Purpose: CSC 17A Project 2
+ *          Basic slot game with added
+ *          cheats and an admin mode
+ * 08 December 2014
+ */
+
+ //System Libraries
+
+ //Published Libraries
+
+ //Namespaces
+ using namespace std;
+
